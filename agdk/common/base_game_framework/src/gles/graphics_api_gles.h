5afd896ae9130bae7a8b48fade9152bfcd4f8ea2
Hakim Hauston <hakimh@google.com>
2024-06-12 17:06:43 +0800

    [ADPF] agdktunnel: added GPU Performance Hint API
    -. api levels + compile sdk version
    -. apply review suggestions + run clang-format
    -. apply review suggestions
    
    Change-Id: I7609bd61c7f9bc8a979425cef1843921fb3c7fb2

--- agdk/common/base_game_framework/src/gles/graphics_api_gles.h
+++ agdk/common/base_game_framework/src/gles/graphics_api_gles.h
@@ -17,16 +17,17 @@
 #ifndef BASEGAMEFRAMEWORK_GRAPHICSAPI_GLES_H_
 #define BASEGAMEFRAMEWORK_GRAPHICSAPI_GLES_H_
 
-#include <cstdint>
 #include <EGL/egl.h>
 #include <GLES3/gl3.h>
+
+#include <cstdint>
 #include <functional>
 #include <memory>
 #include <vector>
 
 #include "graphics_api_base.h"
-#include "graphics_api_status.h"
 #include "graphics_api_gles_resources.h"
+#include "graphics_api_status.h"
 
 namespace base_game_framework {
 
@@ -35,12 +36,16 @@ class GraphicsAPIGLES : public GraphicsAPIBase {
   GraphicsAPIGLES();
   virtual ~GraphicsAPIGLES();
 
-  GraphicsAPIGLES(const GraphicsAPIGLES &) = delete;
-  GraphicsAPIGLES& operator=(const GraphicsAPIGLES &) = delete;
+  GraphicsAPIGLES(const GraphicsAPIGLES&) = delete;
+  GraphicsAPIGLES& operator=(const GraphicsAPIGLES&) = delete;
 
-  virtual DisplayManager::GraphicsAPI GetAPI() const { return DisplayManager::kGraphicsAPI_GLES; }
+  virtual DisplayManager::GraphicsAPI GetAPI() const {
+    return DisplayManager::kGraphicsAPI_GLES;
+  }
   virtual GraphicsAPIStatus GetAPIStatus() const { return api_status_; }
-  virtual const GraphicsAPIFeatures& GetAPIFeatures() const { return api_features_; }
+  virtual const GraphicsAPIFeatures& GetAPIFeatures() const {
+    return api_features_;
+  }
 
   virtual uint32_t GetFeatureFlags() const { return feature_flags_; }
 
@@ -49,40 +54,47 @@ class GraphicsAPIGLES : public GraphicsAPIBase {
   virtual bool InitializeGraphicsAPI();
   virtual void ShutdownGraphicsAPI();
 
-  virtual bool SetDisplayChangedCallback(DisplayManager::DisplayChangedCallback callback,
-                                         void* user_data);
+  virtual bool SetDisplayChangedCallback(
+      DisplayManager::DisplayChangedCallback callback, void* user_data);
 
-  virtual bool SetSwapchainChangedCallback(DisplayManager::SwapchainChangedCallback callback,
-                                           void* user_data);
-  virtual void SwapchainChanged(const DisplayManager::SwapchainChangeMessage message);
+  virtual bool SetSwapchainChangedCallback(
+      DisplayManager::SwapchainChangedCallback callback, void* user_data);
+  virtual void SwapchainChanged(
+      const DisplayManager::SwapchainChangeMessage message);
 
-  virtual DisplayManager::SwapchainConfigurations *GenerateSwapchainConfigurations();
+  virtual DisplayManager::SwapchainConfigurations*
+  GenerateSwapchainConfigurations();
   virtual DisplayManager::InitSwapchainResult InitSwapchain(
-                                    const DisplayManager::DisplayFormat& display_format,
-                                    const DisplayManager::DisplayResolution& display_resolution,
-                                    const DisplayManager::DisplaySwapInterval display_swap_interval,
-                                    const uint32_t swapchain_frame_count,
-                                    const DisplayManager::SwapchainPresentMode present_mode);
+      const DisplayManager::DisplayFormat& display_format,
+      const DisplayManager::DisplayResolution& display_resolution,
+      const DisplayManager::DisplaySwapInterval display_swap_interval,
+      const uint32_t swapchain_frame_count,
+      const DisplayManager::SwapchainPresentMode present_mode);
   virtual void ShutdownSwapchain();
   virtual bool GetSwapchainValid();
   virtual DisplayManager::SwapchainFrameHandle GetCurrentSwapchainFrame();
   virtual DisplayManager::SwapchainFrameHandle PresentCurrentSwapchainFrame();
 
-  bool GetGraphicsAPIResourcesGLES(GraphicsAPIResourcesGLES& api_resources_gles);
+  bool GetGraphicsAPIResourcesGLES(
+      GraphicsAPIResourcesGLES& api_resources_gles);
 
-  bool GetSwapchainFrameResourcesGLES(const DisplayManager::SwapchainFrameHandle frame_handle,
-                                      SwapchainFrameResourcesGLES& frame_resources);
+  bool GetSwapchainFrameResourcesGLES(
+      const DisplayManager::SwapchainFrameHandle frame_handle,
+      SwapchainFrameResourcesGLES& frame_resources);
 
   void LostSurfaceGLES();
   void RestoreSurfaceGLES();
+
  private:
   void QueryCapabilities();
   void ParseEGLConfigs();
   EGLDisplay InitializeEGLDisplay();
-  EGLSurface InitializeEGLSurface(const DisplayManager::DisplayFormat& display_format);
+  EGLSurface InitializeEGLSurface(
+      const DisplayManager::DisplayFormat& display_format);
   EGLContext InitializeEGLContext();
   bool DisplayFormatExists(const DisplayManager::DisplayFormat& display_format);
-  const EGLint *GenerateEGLConfigAttribList(const DisplayManager::DisplayFormat& display_format);
+  const EGLint* GenerateEGLConfigAttribList(
+      const DisplayManager::DisplayFormat& display_format);
 
   std::vector<DisplayManager::DisplayFormat> display_formats_;
   std::vector<DisplayManager::DisplayResolution> display_resolutions_;
@@ -91,13 +103,13 @@ class GraphicsAPIGLES : public GraphicsAPIBase {
   GraphicsAPIStatus api_status_;
   GraphicsAPIFeatures api_features_;
   DisplayManager::DisplayChangedCallback display_changed_callback_;
-  void *display_changed_user_data_;
-  DisplayManager::SwapchainChangedCallback swapchain_changed_callback_ = nullptr;
+  void* display_changed_user_data_;
+  DisplayManager::SwapchainChangedCallback swapchain_changed_callback_ =
+      nullptr;
   void* swapchain_changed_user_data_ = nullptr;
   DisplayManager::GLESFeatureFlags feature_flags_;
   DisplayManager::DisplayFormat swapchain_format_;
   DisplayManager::DisplayResolution swapchain_resolution_;
-  DisplayManager::DisplaySwapInterval swapchain_interval_;
   uint32_t swapchain_frame_count_;
   DisplayManager::SwapchainPresentMode swapchain_present_mode_;
   EGLConfig egl_config_;
@@ -109,6 +121,6 @@ class GraphicsAPIGLES : public GraphicsAPIBase {
   static constexpr const char* BGM_CLASS_TAG = "BGF::GraphicsAPIGLES";
 };
 
-}
+}  // namespace base_game_framework
 
-#endif //BASEGAMEFRAMEWORK_GRAPHICSAPI_GLES_H_
+#endif  // BASEGAMEFRAMEWORK_GRAPHICSAPI_GLES_H_
